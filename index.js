61685e8019591246de1380798fbc6547c808522b
Victor Melo <contatomelo2@gmail.com>
2024-09-01 15:22:35 -0300

    update

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-// script.js
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const allowedEmails = ["autor@gmail.com", "checador@gmail.com", "aprovador@gmail.com", "contatomelo2@gmail.com"];
-    const correctPassword = "mudar123";
-
-    if (allowedEmails.includes(email) && password === correctPassword) {
-        alert("Login bem-sucedido!");
-        window.location.href = "sistema.html"; // Redireciona para o sistema de cadastro de documentos
-    } else {
-        document.getElementById("errorMessage").style.display = "block";
-    }
-});
-
-
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Previne o envio padrão do formulário
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const allowedEmails = ["autor@gmail.com", "checador@gmail.com", "aprovador@gmail.com", "contatomelo2@gmail.com"];
-    const correctPassword = "mudar123";
 
-    if (allowedEmails.includes(email) && password === correctPassword) {
-        // Login autorizado - redireciona para workflow.html
-        window.location.href = 'workflow.html';
-    } else {
-        // Login não autorizado - exibe mensagem de erro
-        document.getElementById("errorMessage").style.display = "block";
-    }
+    // Fazendo uma requisição para o arquivo JSON
+    fetch('users.json')
+        .then(response => response.json())
+        .then(users => {
+            const user = users.find(user => user.email === email && user.password === password);
+            if (user) {
+                // Login autorizado
+                alert("Login bem-sucedido!");
+                window.location.href = 'workflow.html'; // Redireciona para a página do sistema
+            } else {
+                // Login não autorizado - exibe mensagem de erro
+                document.getElementById("errorMessage").style.display = "block";
+            }
+        })
+        .catch(error => {
+            console.error("Erro ao carregar o arquivo JSON:", error);
+        });
 });
